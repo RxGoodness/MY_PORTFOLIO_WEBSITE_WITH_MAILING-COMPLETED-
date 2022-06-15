19094b81c3c7b315e744f6a5cf38d5390d2d86a7
rxgoodness <adedokuntoyyib@gmail.com>
2022-06-16 00:18:05 +0100

    ContactAndExperience Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Card from './components/Card/Card';
+import Contact from './components/Contact/Contact';
+import Experience from './components/Experience/Experience';
 
 function App() {
   return (
     <div className="App">
-      <Card/>
+      {/* <Card/> */}
+      <Contact/>
+      {/* <Experience/> */}
     </div>
   );
 }
