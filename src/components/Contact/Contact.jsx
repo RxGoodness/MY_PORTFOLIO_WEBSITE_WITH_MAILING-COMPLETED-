3beab52f112b4321a2cabb3c5567056a6c556658
rxgoodness <adedokuntoyyib@gmail.com>
2022-06-20 00:21:21 +0100

    addition

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_2mu5xtl",
-        "template_m5udu2c",
+        "SERVICE_ID",
+        "TEMPLATE_ID",
         form.current,
-        "VLwg1ltOWvnCYAiK_"
+        "PUBLIC_KEY"
       )
       .then(
         (result) => {
